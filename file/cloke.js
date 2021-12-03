146643f610da75329ff4d8da1322b367722c1767
hasin3112 <41116183+hasin3112@users.noreply.github.com>
2021-12-03 22:14:07 +0300

    Update cloke.js

--- file/cloke.js
+++ file/cloke.js
@@ -1,16 +1,18 @@
 
 
 function set_cloke() {
-    
+  
 
 var data=new Date();
 var div_time=document.getElementById("div_time");
 div_time.innerHTML= data.getHours() +":"+data.getMinutes()+":"+ data.getSeconds();
 var div_date =document.getElementById("div_date");
 div_date.innerHTML= data.getDate()+"-"+parseInt(data.getMonth()+1)+"-"+ data.getFullYear();
+
+setTimeout(set_cloke, 100)
 };
-setInterval(set_cloke(),50);
-onload =set_cloke();
+
+onload=set_cloke();
 
 function set_color() {
     
@@ -43,7 +45,7 @@ function set_color2() {
       document.getElementById("main2").style.background=localStorage.getItem("set_color2")
       
   }
-  setInterval(update_color2(),100);
+  // setInterval(update_color2(),100);
   onload =update_color2();
   
 
@@ -58,19 +60,25 @@ function set_color2() {
 
 
 
-function scrren_navigtor(){
-  var screen_width = screen.width;
-
- if( screen_width =>500){
+class scrren_navigtor{
+  constructor(){
+  this.screen_width = document.getElementById("main")
+  
+ if(screen.width<500){
 
     
-     document.getElementById("main").style.width="85%";
-     document.getElementById("color2").style.width="85%"
-     document.getElementById("color").style.width="85%"
-     document.getElementById("main").style.marginTop="50px"
+  this.screen_width.style.width=screen.width+"px";
+ 
 
-  
+
+
     }
+    
+
+}
 }
 
- onchange = scrren_navigtor();
+onload =new scrren_navigtor();
+
+
+
